51cde674d1bbde00d02676a357d004bde703f4a2
Mallikharjun Yarra <ymallikharjun@pkrmsezdc.com>
2024-09-10 19:05:37 +0530

    Python Documents

---
+++ pythondocuments/Python_Learning/Python_learn.py
@@ -0,0 +1,21 @@
+pysparks 
+
+Python/Scala --> OOPs Advanced Conceepts  
+Pandas, Numpy , Matplotlib 
+
+Python -->  
+
+Data engineer
+
+Spark with Transformations and Actions 
+(Prasanth kumar pandey)
+Data Bricks 
+
+Pyspark with data Bricks ( Most trending)
+
+Cloud 76
+
+pip3 uninstall sqlalchemy
+pip3 install sqlalchemy==1.4.47
+
+
